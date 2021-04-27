ff8bfa46f7638d35e17f23e64866fc5b2b325b73
YBM PLN PC <bangkarc@gmail.com>
2021-04-27 12:23:19 +0700

    comit pertama

--- app/src/main/java/com/example/uas/MainActivity.java
+++ app/src/main/java/com/example/uas/MainActivity.java
@@ -10,8 +10,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText editText1 ,editText2; //Deklarasi object dari class EdiText
-    String text1 ,text2; //Deklarasi object string
+    EditText editText1 ,editText2;
+    String text1 ,text2;
 
 
     @Override
@@ -24,15 +24,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void loginMasuk(View view) {
-        //Method onClick pada Button
+
 
         editText1 = (EditText)findViewById(R.id.edittext_username);
         editText2 = (EditText) findViewById(R.id.edittext_password);
         text1 = editText1.getText().toString();
         text2 = editText2.getText().toString();
 
-        //Kondisi jika username dan password benar maka akan menampilkan pesan text toast
-        //Login sukses dan masuk ke activity 2
+
         if ((text1.contains("Username"))&&((text2.contains("Password")))) {
             Toast.makeText(this, "Login Sukses", Toast.LENGTH_SHORT).show();
             Intent intent = new Intent(MainActivity.this, SecondActivity.class);
@@ -40,17 +39,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         else if ((text1.matches("")||text2.matches("")))
-          /*
-          Atau jika input text 1 dan text 2 kosong
-           */
+
         {
-            //Maka akan menampilkan pesan text toast
+
             Toast.makeText(this, "Isikan Username dan Password", Toast.LENGTH_SHORT).show();
 
         }
 
         else {
-            //jika kedua kondisi diatas tidak memenuhi
+
 
             Toast.makeText(this, "Login Gagal /Username Password Salah", Toast.LENGTH_SHORT).show();
         }
